576f97ee5f873fe251c87d79b3b1fa6a4216db2e
Giovanni Mori <giovanni_mori@hotmail.com>
2025-06-03 19:52:26 -0300

    parei no lista atividades form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {NavbarComponent} from './_components/navbar/navbar.component';
+import {BaseUiComponent} from './_components/base-ui/base-ui.component';
+import {CertificadosComponent} from './pages/certificados/certificados.component';
+import {CertificadoFormComponent} from './pages/certificado-form/certificado-form.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, BaseUiComponent, CertificadosComponent, CertificadoFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
